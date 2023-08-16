7b9144a9e4a280b36b3d2cb6e3030c1989070055
Marcos Adriano <marcosadriano.ti@gmail.com>
2023-08-16 15:58:33 -0300

    Req 2 done

--- pro_filer/actions/beta_actions.py
+++ pro_filer/actions/beta_actions.py
@@ -20,9 +20,9 @@ def find_file_by_name(context, search_term, case_sensitive=True):
     for path in context["all_files"]:
         file_name = path.split("/")[-1]
 
-        if not case_sensitive:
-            file_name.lower()
-            search_term.lower()
+        if case_sensitive is False:
+            file_name = file_name.lower()
+            search_term = search_term.lower()
 
         if search_term in file_name:
             found_files.append(path)
